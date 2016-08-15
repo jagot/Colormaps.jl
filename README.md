9f9a59fdd3f879355fd1284c17cab6c3c529aa88
Stefanos Carlström <stefanos.carlstrom@fysik.lth.se>
2016-08-15 16:01:02 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Colormaps
 
 [![Build Status](https://travis-ci.org/jagot/Colormaps.jl.svg?branch=master)](https://travis-ci.org/jagot/Colormaps.jl)
+
+Small library to automatically load and register custom colormaps.
--- src/Colormaps.jl
+++ src/Colormaps.jl
@@ -1,5 +1,21 @@
 module Colormaps
 
-# package code goes here
+using PyCall
+@pyimport importlib
+using PyPlot
+@pyimport matplotlib.colors as COL
+
+data_dir = joinpath(Pkg.dir("Colormaps"), "data")
+for file in filter(f -> ismatch(r"\.py$", f), readdir(data_dir))
+    cmap_name = replace(rsplit(file, ".", limit=2)[1], ".", "_")
+    mod = importlib.machinery[:SourceFileLoader](cmap_name,
+                                                 joinpath(data_dir,file))[:load_module]()
+    try
+        plt[:cm][:get_cmap](cmap_name)
+    catch
+        cmap = COL.ListedColormap(mod[:cm_data], name=cmap_name)
+        register_cmap(cmap_name, cmap)
+    end
+end
 
 end # module
